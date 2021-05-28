09770aa34422f41f644891041d37466d78178e7a
sara-carvalho <sarinha@gmail.com>
2021-05-28 08:03:46 -0300

    Initial commit

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+import { Product, products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products = products;
+  flag = false;
+
+  share2() {
+    window.alert('The product has been shared!');
+  }
+
+  share(product: Product) {
+    window.alert(`The ${product.name} has been shared!`);
+  }
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
